977f7b15e1ec97fe8846835a4bd9a6dbb9ae773d
ChenYing Kuo <evshary@gmail.com>
2022-05-10 14:13:17 +0800

    Use correct message type and able to use message filter.

--- src/msg_filter.cpp
+++ src/msg_filter.cpp
@@ -1,19 +1,22 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
-#include "velodyne_msgs/msg/velodyne_scan.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
 #include "message_filters/subscriber.h"
 #include "message_filters/synchronizer.h"
 #include "message_filters/sync_policies/approximate_time.h"
 
 using namespace std;
-using std::placeholders::_1;
 
 class SensorsSubscriber : public rclcpp::Node
 {
 private:
     message_filters::Subscriber<sensor_msgs::msg::Image> _image_sub;
-    message_filters::Subscriber<velodyne_msgs::msg::VelodyneScan> _lidar_sub;
-    void callback(const sensor_msgs::msg::Image::SharedPtr image_msg, const velodyne_msgs::msg::VelodyneScan::SharedPtr lidar_msg) {
+    message_filters::Subscriber<sensor_msgs::msg::PointCloud2> _lidar_sub;
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::PointCloud2> approximate_policy;
+    approximate_policy _approximate_policy;
+    message_filters::Synchronizer<approximate_policy> _syncApproximate;
+
+    void callback(const sensor_msgs::msg::Image::ConstSharedPtr image_msg, const sensor_msgs::msg::PointCloud2::ConstSharedPtr lidar_msg) {
         rclcpp::Time image_ts = image_msg->header.stamp;
         rclcpp::Time lidar_ts = lidar_msg->header.stamp;
         //RCLCPP_INFO(this->get_logger(), "image received timestamp %d s %d ns", image_msg->header.stamp.sec, image_msg->header.stamp.nanosec);
@@ -24,13 +27,15 @@ private:
         RCLCPP_INFO(this->get_logger(), "Time diff (image - lidar) %lf s (%lu ns)", diff_ts.seconds(), diff_ts.nanoseconds());
     }
 public:
-    SensorsSubscriber() : Node("sensors_subscriber") {
+    SensorsSubscriber() : Node("sensors_subscriber"),
+                          _approximate_policy(10),
+                          _syncApproximate(_approximate_policy)
+    {
         _image_sub.subscribe(this, "camera/image_raw");
-        _lidar_sub.subscribe(this, "velodyne_packets");
-        typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, velodyne_msgs::msg::VelodyneScan> approximate_policy;
-        message_filters::Synchronizer<approximate_policy> syncApproximate(approximate_policy(10), _image_sub, _lidar_sub);
-
-        syncApproximate.registerCallback(&SensorsSubscriber::callback, this);
+        _lidar_sub.subscribe(this, "velodyne_points");
+        _syncApproximate.connectInput(_image_sub, _lidar_sub);
+        _syncApproximate.registerCallback(
+            bind(&SensorsSubscriber::callback, this, std::placeholders::_1, std::placeholders::_2));
     }
 };
 
--- src/sensor_subscriber.cpp
+++ src/sensor_subscriber.cpp
@@ -1,6 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
-#include "velodyne_msgs/msg/velodyne_scan.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
 
 using namespace std;
 using std::placeholders::_1;
@@ -8,14 +8,14 @@ using std::placeholders::_1;
 class SensorsSubscriber : public rclcpp::Node {
 private:
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr _image_sub;
-    rclcpp::Subscription<velodyne_msgs::msg::VelodyneScan>::SharedPtr _lidar_sub;
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr _lidar_sub;
 
     void image_callback(const sensor_msgs::msg::Image::SharedPtr image_msg) {
         rclcpp::Time image_ts = image_msg->header.stamp;
         //RCLCPP_INFO(this->get_logger(), "image received timestamp %d s %d ns", image_msg->header.stamp.sec, image_msg->header.stamp.nanosec);
         RCLCPP_INFO(this->get_logger(), "image received timestamp %lf s (%lu ns)", image_ts.seconds(), image_ts.nanoseconds());
     }
-    void lidar_callback(const velodyne_msgs::msg::VelodyneScan::SharedPtr lidar_msg) {
+    void lidar_callback(const sensor_msgs::msg::PointCloud2::SharedPtr lidar_msg) {
         rclcpp::Time lidar_ts = lidar_msg->header.stamp;
         //RCLCPP_INFO(this->get_logger(), "lidar received timestamp %d s %d ns", lidar_msg->header.stamp.sec, lidar_msg->header.stamp.nanosec);
         RCLCPP_INFO(this->get_logger(), "lidar received timestamp %lf s (%lu ns)", lidar_ts.seconds(), lidar_ts.nanoseconds());
@@ -23,7 +23,7 @@ private:
 public:
     SensorsSubscriber() : Node("sensors_subscriber") {
         _image_sub = this->create_subscription<sensor_msgs::msg::Image>("camera/image_raw", 10, std::bind(&SensorsSubscriber::image_callback, this, _1));
-        _lidar_sub = this->create_subscription<velodyne_msgs::msg::VelodyneScan>("velodyne_packets", 10, std::bind(&SensorsSubscriber::lidar_callback, this, _1));
+        _lidar_sub = this->create_subscription<sensor_msgs::msg::PointCloud2>("velodyne_points", 10, std::bind(&SensorsSubscriber::lidar_callback, this, _1));
     }
 };
 
