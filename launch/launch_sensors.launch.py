4e20bfa937e1a3d4badaa652120c15c65bb636e4
ChenYing Kuo <evshary@gmail.com>
2022-05-17 11:34:41 +0800

    Able to launch message filter with IMU supported.

--- launch/launch_sensors.launch.py
+++ launch/launch_sensors.launch.py
@@ -4,11 +4,13 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.conditions import IfCondition
 
 def generate_launch_description():
     camera_type = LaunchConfiguration('camera_type', default='argus')
     open_rviz = LaunchConfiguration('open_rviz', default='false')
+    enable_imu = LaunchConfiguration('enable_imu', default='true')
 
     return LaunchDescription([
         IncludeLaunchDescription(
@@ -23,10 +25,24 @@ def generate_launch_description():
             launch_arguments={'camera_type': camera_type,
                               'open_rviz': open_rviz,}.items(),
         ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                get_package_share_directory('bluespace_ai_xsens_mti_driver'), "/launch/xsens_mti_node.launch.py"
+            ]),
+            condition=IfCondition(PythonExpression(['"', enable_imu, '" == "true"']))
+        ),
+        Node(
+            package='sensors_pkg',
+            executable='msg_filter_with_imu',
+            output='screen',
+            name='msg_filter_with_imu',
+            condition=IfCondition(PythonExpression(['"', enable_imu, '" == "true"']))
+        ),
         Node(
             package='sensors_pkg',
             executable='msg_filter',
             output='screen',
             name='msg_filter',
+            condition=IfCondition(PythonExpression(['"', enable_imu, '" == "false"']))
         ),
     ])
---
+++ src/msg_filter_with_imu.cpp
@@ -0,0 +1,57 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
+#include "sensor_msgs/msg/imu.hpp"
+#include "message_filters/subscriber.h"
+#include "message_filters/synchronizer.h"
+#include "message_filters/sync_policies/approximate_time.h"
+
+using namespace std;
+
+class SensorsSubscriber : public rclcpp::Node
+{
+private:
+    message_filters::Subscriber<sensor_msgs::msg::Image> _image_sub;
+    message_filters::Subscriber<sensor_msgs::msg::PointCloud2> _lidar_sub;
+    message_filters::Subscriber<sensor_msgs::msg::Imu> _imu_sub;
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::PointCloud2, sensor_msgs::msg::Imu> approximate_policy;
+    approximate_policy _approximate_policy;
+    message_filters::Synchronizer<approximate_policy> _syncApproximate;
+
+    void callback(const sensor_msgs::msg::Image::ConstSharedPtr image_msg,
+                  const sensor_msgs::msg::PointCloud2::ConstSharedPtr lidar_msg,
+                  const sensor_msgs::msg::Imu::ConstSharedPtr imu_msg) {
+        rclcpp::Time image_ts = image_msg->header.stamp;
+        rclcpp::Time lidar_ts = lidar_msg->header.stamp;
+        rclcpp::Time imu_ts = imu_msg->header.stamp;
+        //RCLCPP_INFO(this->get_logger(), "image received timestamp %d s %d ns", image_msg->header.stamp.sec, image_msg->header.stamp.nanosec);
+        RCLCPP_INFO(this->get_logger(), "image received timestamp %lf s (%lu ns)", image_ts.seconds(), image_ts.nanoseconds());
+        //RCLCPP_INFO(this->get_logger(), "lidar received timestamp %d s %d ns", lidar_msg->header.stamp.sec, lidar_msg->header.stamp.nanosec);
+        RCLCPP_INFO(this->get_logger(), "lidar received timestamp %lf s (%lu ns)", lidar_ts.seconds(), lidar_ts.nanoseconds());
+        //RCLCPP_INFO(this->get_logger(), "IMU received timestamp %d s %d ns", imu_msg->header.stamp.sec, imu_msg->header.stamp.nanosec);
+        RCLCPP_INFO(this->get_logger(), "IMU received timestamp %lf s (%lu ns)", imu_ts.seconds(), imu_ts.nanoseconds());
+        rclcpp::Duration diff_ts = image_ts-lidar_ts;
+        RCLCPP_INFO(this->get_logger(), "Time diff (image - lidar) %lf s (%lu ns)", diff_ts.seconds(), diff_ts.nanoseconds());
+    }
+public:
+    SensorsSubscriber() : Node("sensors_subscriber"),
+                          _approximate_policy(10),
+                          _syncApproximate(_approximate_policy)
+    {
+        _image_sub.subscribe(this, "camera/image_raw");
+        _lidar_sub.subscribe(this, "velodyne_points");
+        _imu_sub.subscribe(this, "imu/data");
+        _syncApproximate.connectInput(_image_sub, _lidar_sub, _imu_sub);
+        _syncApproximate.registerCallback(
+            bind(&SensorsSubscriber::callback, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+    }
+};
+
+int main(int argc, char ** argv)
+{
+    rclcpp::init(argc, argv);
+    cout << "Start to subscribe lidar, camera and imu topic" << endl;
+    rclcpp::spin(std::make_shared<SensorsSubscriber>());
+    rclcpp::shutdown();
+    return 0;
+}
